41d68323b331dd40a8849b3efaee999567203e65
qinwei <frankqin@gvt861.com>
2019-12-26 16:55:03 +0800

    add api swagger

--- src/Web/app.py
+++ src/Web/app.py
@@ -1,18 +1,21 @@
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
-     render_template
+     render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from Domain.dayquotes import DayQuotes
 from Infrastructure.context import DBContext
+from Web.controllers import stock_controllers
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
+app.register_blueprint(stock_controllers.blueprint_dayquotes)
 Context =DBContext(app)
+swagger=Swagger(app)
 
 @app.route('/')
-def signup():  
+def signup():   
     Context.dayquotesRepository.GetAll()    
     return   render_template("index.html")
-
        
---
+++ src/Web/controllers/stock_controllers.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,Blueprint
+
+blueprint_dayquotes = Blueprint('dayquotes', __name__,template_folder='templates')
+
+@blueprint_dayquotes.route('/dayquotes')
+def Index():
+     """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+     return   render_template("hello.html")
